8567d703d222dab4358dec07fb01fbea930377a8
Yehor M <20585619+satelllte@users.noreply.github.com>
2024-02-27 20:17:36 +0200

    playwright setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import {chromium} from 'playwright'
+import path from 'node:path'
+
+const browser = await chromium.launch({headless: false});
+const page = await browser.newPage();
+const url = `file://${path.resolve(import.meta.dirname, './index.html')}`;
+await page.goto(url);
+await browser.close();
